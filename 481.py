6320d5de3601b0e50d4d8cc951f2bcce5aee2a11
wang1128 <penghao@pal-nat184-026-210.itap.purdue.edu>
2015-12-11 16:20:57 -0500

    improve something

--- 481.py
+++ 481.py
@@ -2,7 +2,7 @@ __author__ = 'penghao'
 from numpy import *
 import csv
 import difflib
-f = open("question.csv")
+f = open("questions.csv")
 csv_f = csv.reader(f)
 question_list =[]
 category_list = []
@@ -15,21 +15,34 @@ def similar(seq1, seq2):
 
 def recommendation(question,category):
     new_question_list=[]
+    rec_list=[]
+    reversed_list=[]
     for idx,cag in enumerate(category_list):
         if category in cag:
 
             new_question_list.append(question_list[idx])
     for element in new_question_list:
         if similar(question,element)>0.5:
-            print(element)
-            print(similar(question,element))
+            rec_list.append((element,similar(question,element)))
 
+    if rec_list.__len__()==0:
+        print ("Sorry, there is no similar questions T-T")
 
-#print(category_list.index("Diet"))
-recommendation("Does obseity relate to race?","Race")
+    return rec_list
 
+def question():
+    que = raw_input('Write your question: ')
+    cag = raw_input('Category: ')
+    recommendation(que,cag)
+    list = recommendation("Does obseity relate to race?","Race")
+    reversed_list = sorted(list, key=lambda list: list[1], reverse=True)
+    print("The top 2 recommendations are :")
+    print(reversed_list[0][0])
+    print(reversed_list[1][0])
+#print(list)
+#list2=sorted(list, key=lambda list: list[1], reverse=True)
+#print(list2)
+#que = raw_input('Write your question: ')
 
-que = raw_input('Wirte your question: ')
-
-cag = raw_input('Category: ')
-recommendation(que,cag)
+#cag = raw_input('Category: ')
+#recommendation(que,cag)
